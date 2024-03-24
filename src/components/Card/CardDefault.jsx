b065d64a70e5d4e9bcfaede6463087b155c9da57
mstsurnalyakter <surnalyms@gmail.com>
2024-03-24 14:34:08 +0600

    category section added

--- src/components/Card/CardDefault.jsx
+++ src/components/Card/CardDefault.jsx
@@ -1,35 +1,56 @@
-import { Button, Card, CardBody, CardFooter, CardHeader, Typography } from '@material-tailwind/react';
-import PropTypes from 'prop-types'
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Typography,
+} from "@material-tailwind/react";
+import PropTypes from "prop-types";
 
-const CardDefault = ({item}) => {
-    console.log(item);
+const CardDefault = ({ item }) => {
+  const {
+    id,
+    title,
+    category,
+    image,
+    description,
+    price,
+    cardBg,
+    textColor,
+    categoryBg,
+  } = item || {};
+  console.log(item);
   return (
-    <Card className="mt-6 ">
-      <CardHeader color="blue-gray" className="relative h-56">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
+    <Card
+      style={{ backgroundColor: cardBg, color: textColor }}
+      className="mt-6 overflow-hidden  cursor-pointer rounded-t-lg rounded-b-none"
+    >
+      <CardHeader color="blue-gray" className="rounded-none m-0">
+        <img className="w-full object-cover" src={image} alt="card-image" />
       </CardHeader>
       <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-          night life in Barcelona.
+        <Button
+          style={{ backgroundColor: categoryBg, color: textColor }}
+          className="mb-4"
+          size="sm"
+        >
+          {category}
+        </Button>
+        <Typography
+          style={{ color: textColor }}
+          variant="h5"
+          color="blue-gray"
+          className="mb-2"
+        >
+          {title}
         </Typography>
       </CardBody>
-      <CardFooter className="pt-0">
-        <Button>Read More</Button>
-      </CardFooter>
     </Card>
   );
-}
+};
 
 CardDefault.propTypes = {
-    item:PropTypes.object.isRequired
-}
+  item: PropTypes.object.isRequired,
+};
 
-export default CardDefault
+export default CardDefault;
--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -5,7 +5,7 @@ import CardDefault from '../Card/CardDefault';
 const CategoryList = () => {
     const { data, loading } = useDonationData();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-6 mx-auto max-w-7xl mt-16">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  gap-6 mx-auto max-w-7xl mt-16 px-10 lg:px-1">
       {data.map((item) => (
         <CardDefault key={item.id} item={item} />
       ))}
