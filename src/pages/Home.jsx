d6f53c5b9aa6f34bc3913ecf1c08150161b77287
mstsurnalyakter <surnalyms@gmail.com>
2024-03-24 08:26:14 +0600

    material tailwind added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import { Button } from "@material-tailwind/react";
 
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
 }
 
 export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-};
+});
